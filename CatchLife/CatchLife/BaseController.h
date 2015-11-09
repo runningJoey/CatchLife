0ac6252148ba86f104932520db6b64208c49b258
lovejmi <imac@lzy.local>
2015-11-09 19:49:20 +0800

    1.1创建了BaseController

---
+++ CatchLife/CatchLife/BaseController.h
@@ -0,0 +1,13 @@
+//
+//  BaseController.h
+//  CatchLife
+//
+//  Created by iMAC on 15/11/9.
+//  Copyright (c) 2015年 iMAC. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface BaseController : ViewController
+
+@end
